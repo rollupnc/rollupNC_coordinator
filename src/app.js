6d83fb0fb07ce03a0cb48e2dad351b33f238e168
vaibhavchellani <vaibhavchellani223@gmail.com>
2019-04-20 17:56:27 +0530

    setup tx receiver

--- src/app.js
+++ src/app.js
@@ -1,17 +1,43 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
-var router = express.Router();
+const config = require("../config/config.js");
 var app = express();
+var amqp = require("amqplib/callback_api");
+
+// environment variables
+process.env.NODE_ENV = "development";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//receives all token transfers
-app.get("/transfer", function(req, res) {
-  // push the transfer to the queue
+// const tx = {
+//   from: pubKey_from[0],
+//   to: pubKey_to[0],
+//   value: amount,
+//   type: token_type_from
+// };
 
-  res.json({ message: "transfer" });
+//receives all token transfers
+app.post("/submit_tx", function(req, res) {
+  console.log("Recieved request", req);
+  // push the transfer to the queue
+  amqp.connect(global.gConfig.amqp, function(err, conn) {
+    conn.createChannel(function(err, ch) {
+      var queue = global.gConfig.tx_queue;
+      ch.assertQueue(queue, { durable: false });
+      ch.sendToQueue(queue, Buffer.from("new msg"));
+      console.log("[x] Sent");
+    });
+    setTimeout(function() {
+      conn.close();
+      process.exit(0);
+    }, 500);
+  });
+  res.json({ message: "Added transfer to queue " });
 });
 
-app.listen(3000);
+app.listen(global.gConfig.port, () => {
+  console.log(
+    `Started listening for transactions on port ${global.gConfig.port} 🎉`
+  );
+});

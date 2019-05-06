57700752a7f516b1986ce446b504889837b849d4
vaibhavchellani <vaibhavchellani223@gmail.com>
2019-05-06 23:57:46 +0530

    working additions to queue

--- src/app.js
+++ src/app.js
@@ -6,75 +6,80 @@ const bigInt = require("snarkjs").bigInt;
 var utils = require("./utils");
 var process_tx = require("./process_tx");
 var amqp = require("amqplib");
-var logger = require("./logger")
+const eddsa = require("../circomlib/src/eddsa.js");
+
+var app = express();
+var logger = require("./logger");
+process.env.NODE_ENV = "development";
 
 const q = global.gConfig.tx_queue;
 const maxTxs = global.gConfig.txs_per_snark;
 var lastPushed = 0;
-var app = express();
-
-
-process.env.NODE_ENV = "development";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //receives all transactions
-app.post("/submit_tx", async function (req, res) {
-  from = req.body.from;
-  to = req.body.to;
-  amount = req.body.amount;
-  tokenType = req.body.tokenType;
-  signature = req.body.signature;
+app.post("/submitTx", async function (req, res) {
+  var fromX = req.body.fromX;
+  var fromY = req.body.fromY;
+  var toX = req.body.toX;
+  var toY = req.body.toY;
+  var amount = req.body.amount;
+  var tokenType = req.body.tokenType;
+  var signature = req.body.signature;
+  console.log("sig", signature)
+  utils.toSignature(signature)
+  // validate if signature is on correct tx hash 
+  txHash = utils.toMultiHash(fromX, fromY, toX, toY, amount, tokenType)
+  // TODO make this work 
+  // if (!utils.checkSignature(txHash, fromX, fromY, signature)) {
+  //   res.status(400).json({ message: "Invalid signature" })
+  //   return
+  // }
 
-  // TODO validate input tx
-
-  // TODO validate transaction
+  // TODO validate tx with cached DB/other contraint checks
 
   // send tx to tx_pool
   await addtoqueue(await utils.getConn());
-  console.log("sent");
+  logger.debug("Added tx to pool")
   res.json({ message: "Added transfer to tx pool " });
 });
 
-app.post('/getTx', function (req, res) {
-  var fromX = BigInt(req.body.fromX);
-  var fromY = BigInt(req.body.fromY);
-  var toX = BigInt(req.body.toX);
-  var toY = BigInt(req.body.toY);
-  var amount = BigInt(req.body.amount);
-  var tokenType = BigInt(req.body.tokenType);
-
-  leafHash = mimcjs.multiHash([
-    fromX, fromY, toX, toY, amount, tokenType
-  ])
-
-  logger.info("Tx leaf hash generated", { tx: leafHash.toString() })
-
-  res.json({ txLeafHash: leafHash.toString() })
+app.post('/getTx', async function (req, res) {
+  var hash = await utils.toMultiHash(req.body.fromX, req.body.fromY, req.body.toX, req.body.toY, req.body.amount, req.body.tokenType).toString()
+  logger.info("Tx leaf hash generated", { tx: hash })
+  res.json({ txLeafHash: hash })
 })
 
-
+// temporary helper api to sign data using given priv key
+app.post("/sign", async function (req, res) {
+  var prvKey = Buffer.from(
+    req.body.privKey.padStart(64, '0'), "hex");
+  var hash = utils.toMultiHash(req.body.fromX, req.body.fromY, req.body.toX, req.body.toY, req.body.amount, req.body.tokenType)
+  signature = eddsa.signMiMC(prvKey, hash);
+  logger.debug("Signature generated", { sig: signature.R8.toString() })
+  res.json({
+    signature: { "R8": signature.R8.toString(), "S": signature.S.toString() }
+  })
+})
 async function addtoqueue(conn) {
   var ch = await conn.createChannel();
   var result = await ch.assertQueue(q);
-  console.log("message count", result);
-  if (result.messageCount - lastPushed > maxTxs) {
-    console.log("clearing queue, starting aggregation");
-    process_tx.fetchTxs();
-    lastPushed = result.messageCount;
-  }
+  logger.debug("Adding new message to queue", { queueDetails: result });
+  // if (result.messageCount - lastPushed > maxTxs) {
+  //   console.log("clearing queue, starting aggregation");
+  //   process_tx.fetchTxs();
+  //   lastPushed = result.messageCount;
+  // }
   await ch.sendToQueue(q, Buffer.from("something to do"));
   return;
 }
 
 app.listen(global.gConfig.port, () => {
-  console.log(
-    `Started listening for transactions on port ${global.gConfig.port} 🎉`
-  );
+  logger.info(
+    "Started listening for transactions", { port: global.gConfig.port })
 });
-
-
 
 process.on("SIGINT", async () => {
   console.log("Received interruption stopping receiver...");
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,8 @@
 var amqp = require("amqplib");
 var conn = null;
+const mimcjs = require("../circomlib/src/mimc7.js");
+const eddsa = require("../circomlib/src/eddsa.js");
+
 
 async function getConn() {
   if (conn === null) {
@@ -7,7 +10,31 @@ async function getConn() {
   }
   return conn;
 }
+
+function toMultiHash(fromX, fromY, toX, toY, amount, token) {
+  leafHash = mimcjs.multiHash([
+    BigInt(fromX), BigInt(fromY), BigInt(toX), BigInt(toY), BigInt(amount), BigInt(token)
+  ])
+  return leafHash
+}
+
+function checkSignature(tx, fromX, fromY, signature) {
+  return eddsa.verifyMiMC(tx, signature,
+    [BigInt(fromX), BigInt(fromY)])
+}
+
+function toSignature(signature) {
+  var sig = {
+    "R8": [BigInt(signature.R8[0].toString())],
+    "S": BigInt(signature.S)
+  }
+  console.log("mew sig", sig)
+  return
+}
 module.exports = {
   getConn,
-  conn
+  conn,
+  toMultiHash,
+  checkSignature,
+  toSignature
 };
